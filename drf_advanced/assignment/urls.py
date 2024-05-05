4002961ae1cfca0bb0ad894984d3a550ef4689e8
seaniiio <cnc4750@naver.com>
2024-05-06 01:37:41 +0900

    feat: api-4 구현

--- drf_advanced/assignment/urls.py
+++ drf_advanced/assignment/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('assignment/<int:pk>',views.AssignmentApiView.as_view())
 ]
--- drf_advanced/assignment/views.py
+++ drf_advanced/assignment/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, JsonResponse
 
-from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import View, APIView
 
+from .serializers import AssignmentSerializer
+from .models import *
 
-def assignment(request):
-    return HttpResponse("assignment server ok!")
+class AssignmentApiView(APIView):
+
+    def get_object(self, pk):
+        assignment = get_object_or_404(Assignment, pk=pk)
+        return assignment
+
+    def get(self, request, pk):
+        assignment = self.get_object(pk)
+
+        assignmentSerializer = AssignmentSerializer(assignment)
+        return JsonResponse(assignmentSerializer.data, status=status.HTTP_200_OK)
