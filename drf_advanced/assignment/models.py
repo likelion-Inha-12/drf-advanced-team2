d6cc1243dbe13868e01251a5fd0d1d9bf92447fb
chanbro0524 <12202049@inha.edu>
2024-05-07 22:57:39 +0900

    models.py complete

--- drf_advanced/assignment/models.py
+++ drf_advanced/assignment/models.py
@@ -10,18 +10,27 @@ class Category(models.Model):
 
 # 과제 table
 class Assignment(models.Model):
+    PART_CHOICES = (
+        ('BE', 'BE'),
+        ('FE', 'FE'),
+        ('ALL', 'ALL'),
+    )
     title = models.CharField(max_length = 100)
     created_at = models.DateTimeField(auto_now_add = True)
     deadline = models.DateTimeField()
-    part = models.CharField(max_length = 10)
+    part = models.CharField(max_length = 10,choices=PART_CHOICES)
     #category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    category = models.CharField(max_length = 20)
+    #category = models.CharField(max_length = 20)
     github_link = models.URLField()
     content = models.TextField()
 
     def __str__(self):
         return self.title
-    
+
+class Category(models.Model):
+    category=models.CharField(max_length=20)
+    assignment_id=models.ForeignKey(Assignment, verbose_name = "subject_category", on_delete=models.CASCADE, related_name="category")
+
 # 제출 table
 class Submission(models.Model):
     assignment_id = models.ForeignKey(Assignment, on_delete=models.CASCADE, related_name="submissions")
--- drf_advanced/assignment/views.py
+++ drf_advanced/assignment/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, JsonResponse
-
+import json
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,6 +9,58 @@ from rest_framework.views import View, APIView
 from .serializers import AssignmentSerializer
 from .models import *
 
+
+#api 1
+def create_assignment(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        title =data.get('title')
+        deadline =data.get('deadline')
+        part =data.get('part')
+        created_at = data.get('created_at')
+        github_link = data.get('github_link')
+        content =data.get('content')
+        
+
+        post =Assignment(
+            title = title,
+            deadline =deadline,
+            part = part,
+            created_at = created_at,
+            github_link = github_link,
+            content = content
+            
+            
+        )
+        post.save()
+        return JsonResponse({'message': 'success'})
+    return JsonResponse({'message':'POST 요청만 허용됩니다'})
+
+#api 2
+def create_submission(request):
+    if request.method == "POST":
+        data =json.loads(request.body)
+
+        description=data.get('description')
+        github_link=data.get('github_link')
+        created_at=data.get('created_at')
+
+        post = Submission(
+            description=description,
+            github_link=github_link,
+            created_at=created_at
+
+        )
+        post.save()
+        return JsonResponse({'message': 'success'})
+    return JsonResponse({'message':'POST 요청만 허용됩니다'})
+
+#api 3
+
+
+
+
+#api 4
 class AssignmentApiView(APIView):
 
     def get_object(self, pk):
